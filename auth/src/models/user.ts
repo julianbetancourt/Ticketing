eac30e1a90f4aed601422ebf5919408c609b9a97
Julian Betancourt <julianbetancourt@Julians-MacBook-Pro.local>
2021-08-08 16:55:41 +0200

    153 - defining extra document properties

---
+++ auth/src/models/user.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+
+interface UserAttrs {
+  email: string
+  password: string
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc
+}
+
+interface UserDoc extends mongoose.Document {
+  email: string
+  password: string
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs)
+}
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema)
+
+const user = User.build({
+  email: "",
+  password: "222",
+})
+
+export { User }
