8e2c913c8d1c3a6215c95cf9c981f3d1ed96faad
Julian Betancourt <julianbetancourt@Julians-MacBook-Pro.local>
2021-08-08 17:10:25 +0200

    156 proper error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express"
 import { body, validationResult } from "express-validator"
+import { BadRequestError } from "../errors/bad-request-error"
 import { DatabaseConnectionError } from "../errors/database-connection-error"
 import { RequestValidationError } from "../errors/request-validation-error"
+import { User } from "../models/user"
 
 const router = express.Router()
 
@@ -14,7 +16,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
@@ -22,10 +24,17 @@ router.post(
     }
     const { email, password } = req.body
 
-    console.log("Creating user")
-    throw new DatabaseConnectionError()
+    const existingUser = await User.findOne({ email })
 
-    res.send({})
+    if (existingUser) {
+      console.log("Email in use")
+      throw new BadRequestError("Email in use")
+    }
+
+    const user = User.build({ email, password })
+    await user.save()
+
+    res.status(201).send(user)
   }
 )
 
