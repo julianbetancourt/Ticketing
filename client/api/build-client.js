7078a476ad40281895ad2aac4d2d2fbafc50e638
Julian Betancourt <julianbetancourt10@gmail.com>
2021-08-17 23:53:33 +0200

    prod dns

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ export default function buildClient({ req }) {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.jobtalks.co/",
       headers: req.headers,
     })
   } else {
