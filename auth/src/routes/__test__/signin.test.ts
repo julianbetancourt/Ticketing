e41cb6478320d2fdda3d96d2c72fbcaf18ec7c21
Julian Betancourt <julianbetancourt10@gmail.com>
2021-08-17 21:22:05 +0200

    purposely broke error

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -8,7 +8,7 @@ it("fails when an email that does not exist is supplied", async () => {
       email: "test@test.com",
       password: "password",
     })
-    .expect(400)
+    .expect(200)
 })
 
 it("fails when an incorrect password is supplied", async () => {
